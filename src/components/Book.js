d1a0aba0d65f85eedcced3d56fd23a9cd2ff84c4
mahmoud_e <54193376+mektefaie@users.noreply.github.com>
2023-11-09 21:30:32 -0800

    finishing JSX - Javascript

---
+++ src/components/Book.js
@@ -0,0 +1,11 @@
+const Book = () => {
+  return (
+    <article className='book'>
+      <img src='./logo192.png' alt='Interesting Facts For Curious Minds' />
+      <h2>Interesting Facts For Create React App</h2>
+      <h4>Jordan Moore </h4>
+    </article>
+  )
+}
+
+export default Book
