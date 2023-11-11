b9e2eedc2d1632cc6db0554dfd62a6ac49cd06f4
mahmoud_e <54193376+mektefaie@users.noreply.github.com>
2023-11-10 16:52:33 -0800

    finishing props

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,17 @@
-const Book = () => {
+const Book = props => {
+  // if passing the entire object as a prop, either use:
+  // const Book = ({ book: { img, title, author } }) => {
+  // or
+  // const { img, title, author } = props.book
+  // or
+
+  const { img, title, author } = props
+
   return (
     <article className='book'>
-      <img src='./logo192.png' alt='Interesting Facts For Curious Minds' />
-      <h2>Interesting Facts For Create React App</h2>
-      <h4>Jordan Moore </h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
     </article>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,28 @@ import ReactDOM from 'react-dom/client'
 import Book from './components/Book'
 import './index.css'
 
+const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Create React App',
+    img: './logo512.png',
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+    id: 2,
+  },
+]
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map(book => {
+        // passing the entire object
+        return <Book {...book} key={book.id} />
+      })}
     </section>
   )
 }
