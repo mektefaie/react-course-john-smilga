20b58debecebf38ff6992e18ec0471c4d3c4bdc6
mahmoud_e <54193376+mektefaie@users.noreply.github.com>
2023-11-12 15:50:19 -0800

    finishing numbers and add title Challenges

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,8 +1,9 @@
 const Book = props => {
-  const { img, title, author, getBook, id } = props
+  const { img, title, author, getBook, id, number } = props
 
   return (
     <article className='book'>
+      <span className='number'>{`# ${number + 1}`}</span>
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <button onClick={() => getBook(id)}>Click me</button>
--- src/index.js
+++ src/index.js
@@ -5,16 +5,15 @@ import Book from './components/Book'
 import { books } from './assets/data/books'
 
 function BookList() {
-  const getBook = id => {
-    console.log(books.find(book => book.id === id))
-  }
-
   return (
-    <section className='booklist'>
-      {books.map(book => {
-        return <Book {...book} key={book.id} getBook={getBook} />
-      })}
-    </section>
+    <>
+      <h1>amazon best sellers</h1>
+      <section className='booklist'>
+        {books.map(book => {
+          return <Book {...book} key={book.id} />
+        })}
+      </section>
+    </>
   )
 }
 
