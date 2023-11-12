51dae79e01b760bc2ac032a44be3c35d419363ac
mahmoud_e <54193376+mektefaie@users.noreply.github.com>
2023-11-12 15:33:45 -0800

    finishing local images i src folder

---
+++ src/assets/data/books.js
@@ -0,0 +1,24 @@
+import img1 from '../images/book-1.webp'
+import img2 from '../images/book-2.webp'
+import img3 from '../images/book-3.webp'
+
+export const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Stephen King',
+    title: 'Fairy Tale',
+    img: img3,
+    id: 3,
+  },
+]
--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Book from './components/Book'
 import './index.css'
-
-const books = [
-  {
-    author: 'Jordan Moore',
-    title: 'Interesting Facts For Create React App',
-    img: './logo512.png',
-    id: 1,
-  },
-  {
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-    id: 2,
-  },
-]
+import Book from './components/Book'
+import { books } from './assets/data/books'
 
 function BookList() {
   const getBook = id => {
